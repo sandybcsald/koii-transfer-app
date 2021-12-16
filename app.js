0b26f5849ca5db426a13977adb42e9fc2ac403ba
Sandeep Singh <singh.sandeep.0173@gmail.com>
2021-12-17 04:24:13 +0530

    SDK Connection Issue Resolved

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-import * as kweb from "@_koi/sdk/web.js";
+const kweb = koi_tools.koi_web;
 const tools = new kweb.Web();
 
 const addr = document.getElementById("address");
 const amt = document.getElementById("amount");
 
 async function transfer() {
-    await tools.loadWallet();
-
+    console.log('hello1');
+    // await tools.loadWallet();
+    console.log('hello2');
     const transferTxId = await tools.transfer(amt, addr, "KOI");
     // number of token, receiver address, type of token(KOI or AR)
     console.log("Your transaction is " + transferTxId);
+    console.log('hello3');
+
 }
 
 function myfunc() { transfer(); }
