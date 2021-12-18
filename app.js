a7567a397863ecae0c68f0ae948a263d4470baa4
Sandeep Singh <singh.sandeep.0173@gmail.com>
2021-12-18 22:56:49 +0530

    working code

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const kweb = koi_tools.koi_web;
 const tools = new kweb.Web();
 
-const addr = document.getElementById("address");
-const amt = document.getElementById("amount");
+// const addr = document.getElementById("address");
+// const amt = document.getElementById("amount");
 
 async function transfer() {
-    console.log('hello1');
-    // await tools.loadWallet();
+    const addr = document.getElementById("address").value;
+    const amt = document.getElementById("amount").value;
     console.log('hello2');
+    console.log(addr, amt);
+    window.koiiWallet.connect();
     const transferTxId = await tools.transfer(amt, addr, "KOI");
     // number of token, receiver address, type of token(KOI or AR)
     console.log("Your transaction is " + transferTxId);
